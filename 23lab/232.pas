8d14b36fa3a535f2b6f4cbf25ff4da6c7e7fc7b5
omgwer <63747048+omgwer@users.noreply.github.com>
2022-05-16 22:02:08 +0300

    upd

---
+++ 23lab/232.pas
@@ -0,0 +1,47 @@
+PROGRAM InsertSort2(INPUT, OUTPUT);
+TYPE 
+  NodePtr = ^Node;
+  Node = RECORD
+           Next: NodePtr;
+           Key: CHAR
+         END;
+VAR
+  FirstPtr, NewPtr, Curr, Prev: NodePtr;
+  Found: BOOLEAN;
+BEGIN {InsertSort2}
+  FirstPtr := NIL;
+  WHILE NOT EOLN(INPUT)
+  DO
+    BEGIN
+      NEW(NewPtr);
+      READ(NewPtr^.Key);
+      Prev := NIL;
+      Curr := FirstPtr;
+      {2.1.1 Найдем значение Prev и Curr, такие что Prev^.Key <= NewPtr^.Key <= Curr^.Key}
+      Found := FALSE;
+      WHILE (Curr <> NIL) AND NOT Found
+      DO
+        IF NewPtr^.Key > Curr^.Key
+        THEN
+          BEGIN
+            Prev := Curr;
+            Curr := Curr^.Next
+          END
+        ELSE
+          Found := TRUE;
+      NewPtr^.Next := Curr;
+      IF Prev = NIL 
+      THEN
+        FirstPtr := NewPtr
+      ELSE
+        Prev^.Next := NewPtr
+    END;
+  NewPtr := FirstPtr;
+  WHILE NewPtr <> NIL
+  DO
+    BEGIN
+      WRITE(NewPtr^.Key);
+      NewPtr := NewPtr^.Next
+    END
+END.  {InsertSort2}
+
